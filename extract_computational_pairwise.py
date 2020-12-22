d0980fd4ee3b74cc8b8fcc437454c9ec907dd343
Andrea Bruera <andrea.bruera@gmail.com>
2020-12-22 07:18:03 +0000

    w2v similarities added

---
+++ extract_computational_pairwise.py
@@ -0,0 +1,30 @@
+import os
+import gensim
+import scipy
+import itertools
+
+### Housekeeping
+
+output_folder = 'computational_models'
+os.makedirs(output_folder, exist_ok=True)
+
+### Reading words
+
+words = []
+
+with open('stimuli_final.csv', 'r') as f:
+    for index, l in enumerate(f):
+        if index != 0:
+            l = str(l).strip().split(';')
+            if l[2] == 'target':
+                words.append(l[0])
+
+word_combs = [k for k in itertools.combinations(words, r=2)]
+
+for i in [50000, 100000, 150000]:
+    with open(os.path.join(output_folder, 'sims_w2v_vocab_{}'.format(i)), 'w') as o:
+        w2v = gensim.models.Word2Vec.load('../../dataset/corpora/itwac/w2v_vocab_{}/w2v_itwac_vocab_{}'.format(i, i))
+        for word_one, word_two in word_combs:
+            current_sim = w2v.similarity(word_one, word_two)
+            current_corr = scipy.stats.pearsonr(w2v.wv[word_one], w2v.wv[word_two])[0]
+            o.write('{}\t{}\t{}\t{}\n'.format(word_one, word_two, current_sim, current_corr))
