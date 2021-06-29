6c81101c7f8f2962e9284a1b9f44db4def83553a
Andrea Bruera <andrea.bruera@gmail.com>
2021-06-29 11:43:59 +0100

    emptying

--- category_classification/lancia_tutto.py
+++
@@ -1,8 +0,0 @@
-import os
-
-analyses = ['subjective_judgments', 'objective_accuracy']
-
-for analysis in analyses:
-    print('python3 time_resolved_classification.py --analysis {}'.format(analysis))
-    os.system('python3 time_resolved_classification.py --analysis {} \
-              --data_folder /import/cogsci/andrea/github/SISSA-EEG/eeg_preprocessing/preprocessed_data/'.format(analysis))
